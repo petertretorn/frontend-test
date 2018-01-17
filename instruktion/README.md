797f7ec3ac11b336961b8965d3d50d5b0beb3e90
Peter Larsen <petertretorn@gmail.com>
2018-01-17 20:53:38 +0100

    bulk commit

---
+++ instruktion/README.md
@@ -0,0 +1,13 @@
+# SKAT front-end test
+
+Siden er forberedt med Bootstrap (v.3) og jQuery men det er op til dig om du vil benytte dig af det.
+
+Byg siden i HTML og CSS ud fra de to screenshots i denne mappe. Billederne er i img mappen, der er en tom CSS fil og en tom JS fil du kan bruge. Teksten er ikke vigtigt men er taget fra https://lipsum.com/ 
+
+Siden skifter fra mobil versionen til fuld skærm ved 768px, læg mærk til forskelligheder.
+
+Footeren skal altid være i bunden af siden (man skal altså scrolle for at se den) men hvis indholdet ikke fylder hele browservinduet skal footeren sidde i bunden af browseren.
+
+Knappen må gerne have en hover effekt hvor de grå striber i baggrunden bytter plads.
+Når man klikker på knappen skal billederne på siden bytte plads, gerne sådan at hvis der var flere billeder, så ville de også blive byttet ud med det efterfølgende billede. 
+
---
+++ js/main.js
@@ -0,0 +1,40 @@
+(function ($) {
+
+    $(document).ready(function () {
+
+        var images = ['SKAT_om_os.jpg', 'SKAT_job.jpg'],
+            index = 0;
+
+        function getNextImage() {
+            var image = images[++index]
+
+            if (!image) {
+                image = images[0];
+                index = 0;
+            }
+            return image;
+        }
+
+        $('#swap-image').click(function () {
+            var background = undefined;
+
+            $('.image').each(function () {
+                //variable redundant now but required if rotating between more than 2 images on page
+                var currentBackground;
+
+                if (background) {
+                    currentBackground = $(this).css('background-image');
+
+                    $(this).css('background-image', background);
+                    background = currentBackground;
+
+                    return;
+                }
+                background = $(this).css('background-image');
+
+                $(this).css('background-image', 'url(./img/' + getNextImage() + ')');
+            });
+        });
+    });
+
+})(jQuery);
